f183466cc95c0bfeffd3a31c734cda2ba0b95e16
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-08-28 21:24:10 +0200

    header lang buttons , changed logo svgs , add fonts

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isHovered = false;
+  
+  selectedLanguage = 'EN';
+
+  selectLanguage(lang: string) {
+    this.selectedLanguage = lang;
+  }
+
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 
 }
