4e9652ca84bc129062f5acfabaddeab0f3db7580
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-01 16:09:39 +0200

    about me section + responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./main-page/main-components/landing-page/landing-page.component";
+import { AboutMeComponent } from "./main-page/main-components/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
