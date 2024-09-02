def266b2f55f16ffd6626eccc8077bd6ed033843
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-02 18:53:13 +0200

    add skills + responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./main-page/main-components/landing-page/landing-page.component";
 import { AboutMeComponent } from "./main-page/main-components/about-me/about-me.component";
+import { SkillsComponent } from './main-page/main-components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent],
+  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
