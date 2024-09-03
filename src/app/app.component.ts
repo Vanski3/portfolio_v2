6d1fa41d285b824d95a3629bec1c7743f69d3423
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-03 21:35:26 +0200

    add portfolio design+ preview hover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from "./main-page/main-components/landing-page/landing-page.component";
 import { AboutMeComponent } from "./main-page/main-components/about-me/about-me.component";
 import { SkillsComponent } from './main-page/main-components/skills/skills.component';
+import { PortfolioComponent } from './main-page/main-components/portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-page/main-components/portfolio/portfolio.component.ts
+++ src/app/main-page/main-components/portfolio/portfolio.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
+  projects = [
+    {
+      index: '01',
+      name: 'Join',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      technologies: ['HTML', 'CSS', 'Firebase', 'JavaScript'],
+      image: './../../../../assets/img/portfolio/join-dialog.png', 
+    },
+    {
+      index: '02',
+      name: 'El Pollo Loco',
+      description: 'A game developed using HTML, CSS, and JavaScript where you need to navigate and avoid obstacles.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image: './../../../../assets/img/portfolio/elp-dialog.png',
+    },
+  ];
 
+  selectedProject = this.projects[0];
+  isDialogOpen = false;
+
+
+  openProject(project: any) {
+    this.selectedProject = project;
+    this.isDialogOpen = true;
+  }
+
+  closeDialog() {
+    this.isDialogOpen = false;
+  }
+
+  nextProject() {
+    const currentIndex = this.projects.indexOf(this.selectedProject);
+    this.selectedProject = this.projects[(currentIndex + 1) % this.projects.length];
+  }
 }
