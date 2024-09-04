9e9b70b11d41363102edd0d1641efd9e168d8e44
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-04 16:23:54 +0200

    portfolio adjustments

--- src/app/main-page/main-components/portfolio/portfolio.component.ts
+++ src/app/main-page/main-components/portfolio/portfolio.component.ts
@@ -14,21 +14,37 @@ export class PortfolioComponent {
       index: '01',
       name: 'Join',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      technologies: ['HTML', 'CSS', 'Firebase', 'JavaScript'],
-      image: './../../../../assets/img/portfolio/join-dialog.png', 
+      technologies: [
+        { name: 'HTML', icon: './../../../../assets/img/portfolio/html-small.svg' },
+        { name: 'CSS', icon: './../../../../assets/img/portfolio/css-small.svg' },
+        { name: 'Firebase', icon: './../../../../assets/img/portfolio/firebase-small.svg' },
+        { name: 'TypeScript', icon: './../../../../assets/img/portfolio/typescript-small.svg' }
+      ],
+      image: './../../../../assets/img/portfolio/join-dialog.png',
+      links: [
+        { name: 'GitHub', url: 'https://github.com/Vanski3/join' },
+        { name: 'Live Test', url: 'https://vanessa-sachs.com/join' }
+      ]
     },
     {
       index: '02',
       name: 'El Pollo Loco',
       description: 'A game developed using HTML, CSS, and JavaScript where you need to navigate and avoid obstacles.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
+      technologies: [
+        { name: 'HTML', icon: './../../../../assets/img/portfolio/html-small.svg' },
+        { name: 'CSS', icon: './../../../../assets/img/portfolio/css-small.svg' },
+        { name: 'JavaScript', icon: './../../../../assets/img/portfolio/javascript-small.svg' }
+      ],
       image: './../../../../assets/img/portfolio/elp-dialog.png',
+      links: [
+        { name: 'GitHub', url: 'https://github.com/Vanski3/elp' },
+        { name: 'Live Test', url: 'https://vanessa-sachs.com/el-pollo-loco' }
+      ]
     },
   ];
 
   selectedProject = this.projects[0];
   isDialogOpen = false;
-
 
   openProject(project: any) {
     this.selectedProject = project;
