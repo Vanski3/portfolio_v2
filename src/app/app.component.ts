a6363b75d352d0d3fe2284e9cb0c29d018a2c23f
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-05 15:13:52 +0200

    splide js install, adjustments for portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LandingPageComponent } from "./main-page/main-components/landing-page/l
 import { AboutMeComponent } from "./main-page/main-components/about-me/about-me.component";
 import { SkillsComponent } from './main-page/main-components/skills/skills.component';
 import { PortfolioComponent } from './main-page/main-components/portfolio/portfolio.component';
+import { ReferencesComponent } from './main-page/main-components/references/references.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
+  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReferencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-page/main-components/references/references.component.ts
+++ src/app/main-page/main-components/references/references.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Splide from '@splidejs/splide';
+
+interface Reference {
+  text: string;
+  name: string;
+  jobTitle: string;
+}
 
 @Component({
   selector: 'app-references',
   standalone: true,
   imports: [],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrls: ['./references.component.scss'],
 })
-export class ReferencesComponent {
+export class ReferencesComponent implements AfterViewInit {
+  references: Reference[] = [
+    {
+      text: "Working with Vanessa on our programming project was a delight. Her humorous and easy-going nature created a positive team environment. Vanessa's ability to maintain a relaxed atmosphere while delivering quality work was truly impressive.",
+      name: "P. Nehlsen",
+      jobTitle: "Team Partner",
+    },
+    {
+      text: "Vanessa consistently demonstrated a high level of dedication and expertise in her work. Her strong team spirit made her a valuable asset to our team. I highly recommend Vanessa.",
+      name: "R. Lochschmidt",
+      jobTitle: "Team Partner",
+    },
+    {
+      text: 'Next reference',
+      name: 'Future Contributor',
+      jobTitle: '',
+    },
+  ];
+
+  ngAfterViewInit(): void {
+    var splide = new Splide('.splide', {
+      type: 'loop',
+      gap: '4rem',
+      focus: 'center',
+      pagination: true,
+      arrows: false, // Verhindert, dass Splide eigene Pfeile generiert
+      padding: {
+        left: '20%',
+        right: '20%',
+      },
+      breakpoints: {
+        960: {
+          gap: '2rem',
+        },
+        768: {
+          gap: '0.1rem',
+          padding: {
+            left: '3%',
+            right: '5%',
+          },
+        },
+      },
+    });
+  
+    document.querySelector('.custom-arrow--prev')!.addEventListener('click', () => {
+      splide.go('<');
+    });
+  
+    document.querySelector('.custom-arrow--next')!.addEventListener('click', () => {
+      splide.go('>');
+    });
+  
+    splide.mount();
+  }
+  
 
 }
