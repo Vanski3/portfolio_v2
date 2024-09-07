5473c7346eb96d0443d8b22c891c74e17d772695
Vanski3 <122392034+Vanski3@users.noreply.github.com>
2024-09-07 21:07:48 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { SkillsComponent } from './main-page/main-components/skills/skills.compo
 import { PortfolioComponent } from './main-page/main-components/portfolio/portfolio.component';
 import { ReferencesComponent } from './main-page/main-components/references/references.component';
 import { ContactComponent } from './main-page/main-components/contact/contact.component';
+import { FooterComponent } from './main-page/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReferencesComponent, ContactComponent],
+  imports: [RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ReferencesComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
